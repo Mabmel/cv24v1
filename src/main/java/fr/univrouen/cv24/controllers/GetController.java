b746f8bda6e63fe4258e51b616e91049b26e61e7
melissa <melissa06abbas@gmail.com>
2024-04-03 14:43:36 +0200

    commit tp7

---
+++ src/main/java/fr/univrouen/cv24/controllers/GetController.java
@@ -0,0 +1,44 @@
+package fr.univrouen.cv24.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.univrouen.cv24.models.TestCV;
+import util.Fichier;
+
+@RestController
+public class GetController {
+
+	@GetMapping("/resume")
+	public String getListCVinXML() {
+		return "Envoi de la liste des CV";
+	}
+
+	@GetMapping("/cvid")
+	public String getCVinXML(@RequestParam(value = "texte") String texte) {
+		return ("Détail du CV n°" + texte);
+	}
+
+	@GetMapping("/test")
+	public String testIdTitre(@RequestParam(value = "id") int id, @RequestParam(value = "titre") String titre) {
+		return "Test :\n id = " + id + "\n titre = " + titre;
+	}
+
+	@GetMapping("/testfic")
+	public String afficherContenuFichier() {
+
+		return Fichier.loadFileXML();
+
+	}
+
+	@RequestMapping(value = "/testxml", produces = MediaType.APPLICATION_XML_VALUE)
+	public @ResponseBody TestCV getXML() {
+		TestCV cv = new TestCV("HAMILTON", "Margaret", "1969/07/21", "Appollo11@nasa.us");
+		return cv;
+	}
+
+}
